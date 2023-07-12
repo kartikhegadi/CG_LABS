65a52b70320baf0f9e910fa4bd7bacd393cea5d4
Kartik Hegadi <67619115+kartikhegadi@users.noreply.github.com>
2023-07-12 11:41:14 +0530

    one modifies
    modified one

--- one.main.cpp
+++ one.main.cpp
@@ -1,66 +1,149 @@
- #include<stdio.h>
-#include<math.h>
 #include<GL/glut.h>
-GLint X1,Y1,X2,Y2;
-void LineBres(void)
+#include<stdio.h>
+#include<GL/glu.h>
+int x2,x1,y1,y2;
+
+void draw(int x,int y)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	int dx=abs(X2-X1),dy=abs(Y2-Y1);
-	int p=2*dy-dx;
-	int twoDy=2*dy,twoDyDx=2*(dy-dx);
-	int x,y;
-	if(X1>X2)
-	{
-		x=X2;
-		y=Y2;
-		X2=X1;
-	}
-	else
-	{
-		x=X1;
-		y=Y1;
-		X2=X2;
-	}
-	glBegin(GL_POINTS);
-	glVertex2i(x,y);
-	while(x<X2)
-	{
-		x++;
-		if(p<0)
-			p+=twoDy;
-		else
-		{
-			y++;
-			p+=twoDyDx;
-		}
-		glVertex2i(x,y);
-	}
-	glEnd();
-	glFlush();
+
+    glColor3i(1,0,0);
+    glBegin(GL_POINTS);
+        glVertex2i(x,y);
+    glEnd();
 }
-void Init()
+
+
+void init()
 {
-	glClearColor(1.0,1.0,1.0,0);
-	glColor3f(0.0,0.0,0.0);
-	glPointSize(8.0);
-	glViewport(0,0,50,50);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0,50,0,50);
+
+    glClearColor(1,1,1,1);
+    gluOrtho2D(0.0, 500.0 ,0.0 ,500.0);
 }
-int main(int argc,char **argv)
+void linedraw(int x1,int y1,int x2,int y2)
 {
-	printf("Enter wo points for drawing line Bresenham:\n");
-	printf("\n enter points (X1,Y1):");
-	scanf("%d %d",&X1,&Y2);
-	printf("\n enter points (X2,Y2):");
-	scanf("%d %d",&X2,&Y2);
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(300,400);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("LINE BRESENHAM :C Tathva");
-	Init();
-	glutDisplayFunc(LineBres);
-	glutMainLoop();
+    int dx=x2-x1;
+    int dy=y2-y1;
+    int m=dy/dx;
+
+    if(m<1)
+    {
+        int dp=2*dy-dx;
+        int x=x1;
+        int y=y1;
+        if(dx<0)
+        {
+            x=x2;
+            y=y2;
+            x2=x1;
+        }
+        draw(x,y);
+        while(x<x2)
+        {
+
+            if(dp>=0)
+            {
+
+                x=x+1;
+                y=y+1;
+                dp=dp+2*dy-2*dx*(y+1-y);
+            }
+            else
+            {
+                x=x+1;
+                y=y;
+                dp=dp+2*dy-2*dx*(y-y);
+
+            }
+             draw(x,y);
+        }
+
+    }
+    else if (m>1)
+    {
+
+        int dp=2*dy-dx;
+        int x=x1;
+        int y=y1;
+
+
+        if(dy<0)
+        {
+            x=x2;
+            y=y2;
+            y2=y1;
+        }
+        draw(x,y);
+        while(y<y2)
+        {
+
+            if(dp>=0)
+            {
+
+                x=x+1;
+                y=y+1;
+                dp=dp+2*dy-2*dx*(x+1-x);
+            }
+            else
+            {
+                y=y+1;
+                x=x;
+                dp=dp+2*dy-2*dx*(x-x);
+
+            }
+             draw(x,y);
+
+
+        }
+     }
+
+    else if(m==1)
+    {
+        int x=x1;
+        int y=y1;
+        draw(x,y);
+        while(x<x2)
+        {
+
+            x=x+1;
+            y=y+1;
+            draw(x,y);
+        }
+
+
+    }
+
+}
+void display()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    linedraw(x1,y1,x2,y2);
+
+    glFlush();
+
+}
+
+int main(int argc , char **argv)
+{
+
+    printf("Enter the first point (x1,y2)");
+    scanf("%d%d",&x1,&y2);
+
+    printf("Enter the first point (x2,y2)");
+    scanf("%d%d",&x2,&y2);
+
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(220,220);
+    glutCreateWindow("prog 1");
+
+    init();
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+
+
 }
